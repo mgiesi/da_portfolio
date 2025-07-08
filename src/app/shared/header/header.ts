8a433403b6d4fc9cc7d0751a1d70608a56d6aee8
Markus Giesinger <mgiesi@gmail.com>
2025-07-08 13:36:12 +0200

    Update styles and implement header UI

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class Header {
-
+  constructor(private translate: TranslateService) {
+    
+  }
 }
