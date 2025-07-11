e395bd9fa288e6c860de8cdf83ed5a7d7f7f5cc4
Markus Giesinger <mgiesi@gmail.com>
2025-07-11 09:21:18 +0200

    Add new component for legal notice and main content. Also update UI elements in footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LegalNotice } from './legal-notice/legal-notice';
+import { MainContent } from './main-content/main-content';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: MainContent },
+    { path: 'legalnotice', component: LegalNotice }
+];
