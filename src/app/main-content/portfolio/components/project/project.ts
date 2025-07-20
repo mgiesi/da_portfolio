53394055328ec3b9ffaf63329231ea8642bba1b5
Markus Giesinger <mgiesi@gmail.com>
2025-07-20 20:09:07 +0200

    Update responsive layout of portfolio section

--- src/app/main-content/portfolio/components/project/project.ts
+++ src/app/main-content/portfolio/components/project/project.ts
@@ -1,14 +1,17 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-project',
-  imports: [TranslateModule],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './project.html',
   styleUrl: './project.scss'
 })
 export class Project {
   @Input() project = '';
+  @Input() align: 'left' | 'right' = 'left';
+  @Input() skills: string[] = [];
 
   constructor(private translate: TranslateService) {
   }
