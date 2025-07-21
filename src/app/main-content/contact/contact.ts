dbd1dd2b61928bd8876b6563d27ecf01647f2061
Markus Giesinger <mgiesi@gmail.com>
2025-07-21 21:27:02 +0200

    Update UI for contact section

--- src/app/main-content/contact/contact.ts
+++ src/app/main-content/contact/contact.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { TranslateService, TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [TranslateModule],
+  imports: [TranslateModule, CommonModule, FormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss'
 })
 export class Contact {
   constructor(private translate: TranslateService) {
+  }
+
+  markAsTouched(input: any) {
+    if (!input.touched) {
+      input.control.markAsTouched();
+    }
   }
 }
