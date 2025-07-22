d4650269f8288256ec9481c0f8d042be561d7881
Markus Giesinger <mgiesi@gmail.com>
2025-07-22 23:10:25 +0200

    Add new privacy policy page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LegalNotice } from './legal-notice/legal-notice';
 import { MainContent } from './main-content/main-content';
+import { PrivacyPolicy } from './privacy-policy/privacy-policy';
 
 export const routes: Routes = [
     { path: '', component: MainContent },
-    { path: 'legalnotice', component: LegalNotice }
+    { path: 'legalnotice', component: LegalNotice },
+    { path: 'privacy', component: PrivacyPolicy }
 ];
