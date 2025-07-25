85bda8165429e8f8b39abfa6bd008b621ebd40c2
Markus Giesinger <mgiesi@gmail.com>
2025-07-25 20:23:47 +0200

    Add first draft of Feedback section

--- src/app/main-content/portfolio/portfolio.ts
+++ src/app/main-content/portfolio/portfolio.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TranslateService, TranslateModule } from '@ngx-translate/core';
 import { Project } from './components/project/project';
+import { Feedback } from './components/feedback/feedback';
 
 @Component({
   selector: 'app-portfolio',
-  imports: [TranslateModule, Project],
+  imports: [TranslateModule, Project, Feedback],
   templateUrl: './portfolio.html',
   styleUrl: './portfolio.scss'
 })
