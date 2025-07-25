2abbca8f1a3ffbc9531478a26fbdf9362040d74e
Markus Giesinger <mgiesi@gmail.com>
2025-07-25 19:32:34 +0200

    Add translation texts to legal notice and privacy policy page

--- src/app/legal-notice/legal-notice.ts
+++ src/app/legal-notice/legal-notice.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.scss'
 })
 export class LegalNotice {
-
+  constructor(public translate: TranslateService) { }
 }
--- src/app/privacy-policy/privacy-policy.ts
+++ src/app/privacy-policy/privacy-policy.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './privacy-policy.html',
   styleUrl: './privacy-policy.scss'
 })
 export class PrivacyPolicy {
+  constructor(public translate: TranslateService) { }
 
 }
